a359824766dc0a2220285a9ea1a933b9aa4abc86
virtusainternsip <virtusainternsip@gmail.com>
2022-03-09 09:39:50 +0300

    first commit

---
+++ src/components/AddEmployeeComponent.js
@@ -0,0 +1,130 @@
+import React, {useState, useEffect} from 'react'
+import {Link, useHistory, useParams } from 'react-router-dom';
+import EmployeeService from '../services/EmployeeService'
+
+const AddEmployeeComponent = () => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [emailId, setEmailId] = useState('')
+    const history = useHistory();
+    const {id} = useParams();
+
+    const saveOrUpdateEmployee = (e) => {
+        e.preventDefault();
+
+        const employee = {firstName, lastName, emailId}
+
+        if(id){
+            EmployeeService.updateEmployee(id, employee).then((response) => {
+                history.push('/employees')
+            }).catch(error => {
+                console.log(error)
+            })
+
+        }else{
+            EmployeeService.createEmployee(employee).then((response) =>{
+
+                console.log(response.data)
+    
+                history.push('/employees');
+    
+            }).catch(error => {
+                console.log(error)
+            })
+        }
+        
+    }
+
+    useEffect(() => {
+
+        EmployeeService.getEmployeeById(id).then((response) =>{
+            setFirstName(response.data.firstName)
+            setLastName(response.data.lastName)
+            setEmailId(response.data.emailId)
+        }).catch(error => {
+            console.log(error)
+        })
+    }, [])
+
+    const validTextRegex = RegExp(
+        /^(?!\s)[A-Za-z\s]+$/
+      );
+      
+    const title = () => {
+
+        if(id){
+            return <h2 className = "text-center">Update Employee</h2>
+        }else{
+            return <h2 className = "text-center">Add Employee</h2>
+        }
+    }
+
+    return (
+        <div>
+           <br /><br />
+           <div className = "container">
+                <div className = "row">
+                    <div className = "card col-md-6 offset-md-3 offset-md-3">
+                       {
+                           title()
+                       }
+                        <div className = "card-body">
+                            <form onSubmit={(e) => saveOrUpdateEmployee(e)}> 
+                                <div className = "form-group mb-2">
+                                    <label className = "form-label"> First Name :</label>
+                                    <input
+                                        type = "text"
+                                        placeholder = "Enter first name"
+                                        name = "firstName"
+                                        className = "form-control"
+                                        required
+                                        value = {firstName}
+                                        onChange = {(e) => setFirstName(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+
+                                <div className = "form-group mb-2">
+                                    <label className = "form-label"> Last Name :</label>
+                                    <input
+                                        type = "text"
+                                        placeholder = "Enter last name"
+                                        name = "lastName"
+                                        className = "form-control"
+                                        required
+                                        value = {lastName}
+                                        onChange = {(e) => setLastName(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+
+                                <div className = "form-group mb-2">
+                                    <label className = "form-label"> Email Id :</label>
+                                    <input
+                                        type = "email"
+                                        placeholder = "Enter email Id"
+                                        name = "emailId"
+                                        className = "form-control"
+                                        value = {emailId}
+                                        required
+                                        onChange = {(e) => setEmailId(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+
+                                <button className = "btn btn-success" type="submit" >Submit </button>
+                                <Link to="/employees" className="btn btn-danger"> Cancel </Link>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+
+           </div>
+
+        </div>
+    )
+}
+
+export default AddEmployeeComponent
