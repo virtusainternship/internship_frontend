4291b5aae0e85e7b7b2fe553ec09f1f9652c4008
virtusainternship <virtusainternship@gmail.com>
2022-05-18 16:58:29 +0300

    intergration

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -388,7 +388,7 @@ const PersonalInformation = () => {
                     <div className="col-md-4">
                         <Form.Group controlId="dob">
                             <Form.Label>Select Date</Form.Label>
-                            <Form.Control type="date" name="dob" value={dob} placeholder="Date of Birth" />
+                            <Form.Control type="date" name="dob" placeholder="Date of Birth" />
                         </Form.Group>
                     </div>
                 </div>
